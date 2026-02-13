136c7904d0c178cd561cfcccd60a9f3f56c26f18
Driton Jasiqi <jasiqidriton@gmail.com>
2026-02-14 00:30:21 +0100

    Add some Features to Sidebar,a lot more to be done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 
-import React from 'react';
+import React, { useState} from 'react';
 import './App.css';
 import Sidebar from './components/layout/Sidebar';
 import Header from './components/layout/Header';
 
 function App() {
+
+  const  [sidebarCollapsed, setSidebarCollapsed] = useState(false);
+  const [currectPage, setCurrentPage] = useState("dashboard");
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50  transition-all duration-500 ">
       <div className="flex h-screen overflow-hidden">
-        <Sidebar />
+        <Sidebar collapsed = {sidebarCollapsed} onToggle={() => setSidebarCollapsed(!sidebarCollapsed)}
+        currentPage =  {currectPage} 
+        onPageChange = {setCurrentPage}/>
         <div className='flex-1 flex flex-col overflow-hidden'>
           <Header />
         </div>
--- frontend/src/components/layout/Sidebar.jsx
+++ frontend/src/components/layout/Sidebar.jsx
@@ -1,5 +1,5 @@
-import { Zap, LayoutDashboard, Users,Settings, ChevronDown } from "lucide-react";
-import React from "react";
+import { Zap, LayoutDashboard, Users, Settings, ChevronDown } from "lucide-react";
+import React,{useState} from "react";
 
 const menuItems = [
     {
@@ -7,13 +7,13 @@ const menuItems = [
         icon: LayoutDashboard,
         label: "Dashboard",
         active: true,
-        badge : "New"
+        badge: "New"
     },
     {
         id: "users",
         icon: Users,
         label: "Users",
-        badge : "0",
+        badge: "0",
         submenu: [
             { id: "all-users", label: "All Users" },
             { id: "roles", label: "Roles" },
@@ -21,10 +21,23 @@ const menuItems = [
         ],
     }
 ];
-function Sidebar() {
-    return (
-        <div className="w-72 transition duration-300 ease-in-out bg-white/95 backdrop-blur-xl  flex flex-col relative z-10">
+function Sidebar({ collapsed, onToggle, currentPage, onPageChange }) {
 
+    const [expandedItem, setExpandedItem] = useState(new Set(['analytics']));
+
+    const toggleExpanded = (itemid) => {
+        const newExpanded = new Set(expandedItem);
+        if (newExpanded.has(itemid)) {
+            newExpanded.delete(itemid);
+        } else {
+            newExpanded.add(itemid);
+        }
+        setExpandedItem(newExpanded);
+    };
+
+
+    return (
+        <div className={`${collapsed ? "w-20" : "w-72"} transition duration-300 ease-in-out bg-white/95 backdrop-blur-xl  flex flex-col relative z-10`}>
             {/* Logo*/}
             <div className="p-6">
                 <div className="flex items-center space-x-3">
@@ -32,51 +45,64 @@ function Sidebar() {
                         <Zap className="w-6 h-6 text-white" />
                     </div>
                     {/*Conditional Rendering */}
-                    <div>
+                    {!collapsed && (<div>
                         <h1 className="text-xl font-bold text-slate-800 ">
                             Name
                         </h1>
                         <p className="text-xs text-slate-500">
                             Panel Type
                         </p>
-                    </div>
+                    </div>)}
                 </div>
             </div>
 
             {/*Navigation */}
             <nav className="flex-1 p-4 space-y-2 overflow-y-auto">
-               {menuItems.map((item) => (
-                <div key={item.id}>
-                    <button className={'w-full flex items-center justify-between p-3 rounded-xl transition-all duration-200 hover:bg-slate-100 '}>
-                        <div className="flex items-center space-x-3">
-                            <item.icon className="w-5 h-5 text-gray-600" />
-                            {/*Conditional Rendering */}
-                            <>
-                            <span className="font-medium ml-2 bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">{item.label}</span>
-                            <span className="px-2 py-1 text-xs bg-slate-800 text-white rounded-full">
-                                {item.badge}
-                            </span>
-                            </>
-                        </div>
+                {menuItems.map((item) => (
+                    <div key={item.id}>
+                        <button 
+                        className={`w-full flex items-center justify-between p-3 rounded-xl transition-all duration-200  ${currentPage === item.id || item.active ? "bg-gradient-to-r from-blue-600 to-purple-600 text-white shadow-lg shadow-blue-500/25" : "text-gray-600 hover:bg-slate-100"
+                        }`}
+                        onClick={() => {
+                            if(item.submenu){
+                                toggleExpanded(item.id)
+                            }else{
+                                onPageChange(item.id)
+                            }
+                        }}  
+                        >
+                            <div className="flex items-center space-x-3">
+                                <item.icon className="w-5 h-5 text-gray-600" />
+                                {/*Conditional Rendering */}
+                                <>
+                                    {!collapsed && (
+                                        <span className="font-medium ml-2 bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text ">{item.label}</span>
+                                    )}
+                                    {item.badge && (
+                                        <span className="px-2 py-1 text-xs bg-slate-200 text-slate-600 rounded-full">
+                                            {item.badge}
+                                        </span>
+                                    )}
+                                </>
+                            </div>
 
-                        {item.submenu && (
-                            <ChevronDown className="w-4 h-4 text-gray-400 transition-transform "/>
-                        )}
-                    </button>
-
-                    {/* Submenu */}
-                    {/* <div className="ml-8 mt-2 space-y-1">
-                      {item.submenu && item.submenu.map((sub) => (
-                        <button className="block w-full text-left px-3 py-2 text-sm text-slate-600 hover:bg-slate-100 rounded-lg transition-colors duration-200">
-                            {sub.label}
+                            {!collapsed && item.submenu && (
+                                <ChevronDown className={`w-4 h-4 text-gray-400 transition-transform`} />
+                            )}
                         </button>
-                      ))}    
-                    </div>                 */}
-                </div>
-            ))}
+
+                        {/* Submenu */}
+                        {!collapsed && item.submenu && expandedItem.has(item.id) && (
+                            <div className="ml-8 mt-2 space-y-1">
+                                { item.submenu.map((sub) => (<button className="w-full text-left p-2 text-sm text-slate-600 hover:bg-slate-100 rounded-lg">{sub.label}</button>))}
+                            </div>
+                        )}
+                    </div>
+                ))}
             </nav>
 
-            <div className="p-4 border-t border-slate-200/50 ">
+            {!collapsed && (
+                <div className="p-4 border-t border-slate-200/50 ">
                 <div className="flex items-center space-x-3 p-3 rounded-xl bg-slate-50">
                     <img
                         src="https://png.pngtree.com/png-vector/20191110/ourmid/pngtree-avatar-icon-profile-icon-member-login-vector-isolated-png-image_1978396.jpg"
@@ -95,6 +121,7 @@ function Sidebar() {
                     </div>
                 </div>
             </div>
+            )}
         </div>
     );
 }
