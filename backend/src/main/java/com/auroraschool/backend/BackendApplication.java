b31e6ea3dab10f270c2c8640d1ad4954a73d6a07
Driton Jasiqi <jasiqidriton@gmail.com>
2026-04-17 01:56:23 +0200

    restarted backend

--- backend/src/main/java/com/auroraschool/backend/BackendApplication.java
+++ backend/src/main/java/com/auroraschool/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.sms.backend;
+package com.auroraschool.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/auroraschool/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/auroraschool/backend/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sms.backend;
+package com.auroraschool.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
