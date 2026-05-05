a7cb5824caef7355c298ff8d88f2472f75988195
Driton Jasiqi <jasiqidriton@gmail.com>
2026-05-05 23:10:37 +0200

    Edited Student.java. Added Property enrollmentNUmber, Added necessary Annotations. Add the logic for classes but its commented till the Classes.java implemented ist

--- backend/src/main/java/com/auroraschool/backend/model/Student.java
+++ backend/src/main/java/com/auroraschool/backend/model/Student.java
@@ -1,4 +1,20 @@
 package com.auroraschool.backend.model;
 
-public class Student {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@PrimaryKeyJoinColumn(name = "user_id") // verweist auf einen User
+public class Student extends User{
+    @Column(unique = true, nullable = false)
+    private Long enrollmentNUmber;
+
+    //@OneToMany(cascade = CascadeType.ALL,mappedBy = "student", orphanRemoval = true)
+    //private List<Classes> classes = new ArrayList<>();
 }
