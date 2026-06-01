8155500ef4b88568ad21c6ec209cdcb47fc5ce0b
jasiq <jasiqidriton@gmail.com>
2026-06-01 13:04:04 +0200

    Implemented in AuthController the register Student mapping

---
+++ backend/src/main/java/com/auroraschool/backend/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.auroraschool.backend.controller;
+
+import com.auroraschool.backend.exception.EmailExistException;
+import com.auroraschool.backend.model.Professor;
+import com.auroraschool.backend.model.Student;
+import com.auroraschool.backend.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserService userService;
+
+    @PostMapping("/register/Student")
+    public ResponseEntity<?> registerStudent(@RequestBody Student student) {
+        try {
+            Student createdStudent = userService.addStudent(student);
+            return  ResponseEntity.status(HttpStatus.CREATED).body(createdStudent);
+        } catch (EmailExistException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while registering the student.");
+        }
+    }
+}
