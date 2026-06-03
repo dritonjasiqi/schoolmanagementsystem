1093a7157b3677379f4812e25d3013ea4bd82109
jasiq <jasiqidriton@gmail.com>
2026-06-03 14:29:28 +0200

    Add JavaDoc at Admin.java Class

--- backend/src/main/java/com/auroraschool/backend/model/Admin.java
+++ backend/src/main/java/com/auroraschool/backend/model/Admin.java
@@ -1,15 +1,27 @@
 package com.auroraschool.backend.model;
 
+/*
+ * Package provides the necessary annotations used to define the relational
+ * mapping between this Java Class and underlying ProgressSql Database via
+ * the Persistence Provider
+ */
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Entity representing an Admin in the Aurora School system. This class extends the abstract User class.
+ * It includes specific attributes relevant to admin, such as clearanceLevel.
+ */
 @Entity
 @Table(name = "admins")
 @PrimaryKeyJoinColumn(name = "user_id")
 @Getter
 @Setter
 public class Admin extends User{
+    /**
+     * Clearance level of the admin, used to determine the level of access and permissions within the system.
+     */
     @Column(nullable = false)
     private Integer clearanceLevel = 1;
 }
