9b0b6dc2e087c2f863f1b4dc27a38b5eee739a55
jasiq <jasiqidriton@gmail.com>
2026-06-04 23:12:50 +0200

    Add JavaDoc at CourseService.java Interface

--- backend/src/main/java/com/auroraschool/backend/service/CourseService.java
+++ backend/src/main/java/com/auroraschool/backend/service/CourseService.java
@@ -5,10 +5,27 @@ import com.auroraschool.backend.model.Course;
 import java.util.UUID;
 import java.util.List;
 
+/**
+ * Interface that defines the methods for the CourseServiceClass
+ */
 public interface CourseService {
+    /**
+     * Creates a Course and associate it to a professor
+     * @param course Course Object
+     * @param professorId Id of the professor to associate the course to
+     * @return the created Course
+     */
     Course createCourse(Course course, UUID professorId);
 
+    /**
+     * Returns the Course
+     * @param courseID Id of the course to search
+     * @return the Course associated to the courseID
+     */
     Course getCourse(UUID courseID);
 
+    /**
+     * @return All the courses stored in the Database
+     */
     List<Course> getAllCourses();
 }
