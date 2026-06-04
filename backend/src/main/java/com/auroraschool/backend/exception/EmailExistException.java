3a5033a51a6234c40cd050c8eb88bed65101d675
jasiq <jasiqidriton@gmail.com>
2026-06-04 23:30:58 +0200

    Add JavaDoc at EmailExistException.java

--- backend/src/main/java/com/auroraschool/backend/exception/EmailExistException.java
+++ backend/src/main/java/com/auroraschool/backend/exception/EmailExistException.java
@@ -1,5 +1,8 @@
 package com.auroraschool.backend.exception;
 
+/**
+ * Exception thrown when trying to register a user with an email that already exists in the database
+ */
 public class EmailExistException extends IllegalArgumentException {
     public EmailExistException(String message) {
         super(message);
