fa8e6b2b5fe1886ff0b7162cc56b3676bc85194b
jasiq <jasiqidriton@gmail.com>
2026-06-04 23:25:09 +0200

    Add JavaDoc at UserService.java Interface

--- backend/src/main/java/com/auroraschool/backend/service/UserService.java
+++ backend/src/main/java/com/auroraschool/backend/service/UserService.java
@@ -8,12 +8,54 @@ import com.auroraschool.backend.model.Student;
 import com.auroraschool.backend.model.User;
 
 public interface UserService {
+    /**
+     * Remove User by Id
+     * @param id Id of the user to remove
+     */
     void removeUser(UUID id);
+
+    /**
+     * Gives the User associated with Username
+     * @param username Username of the user that its being search
+     * @return User
+     */
     User getUserByUsername(String username);
+    /**
+     * Gives the User associated with Email
+     * @param email Email of the user that its being search
+     * @return User
+     */
     User getUserByEmail(String email);
+    /**
+     * Gives the User associated with Id
+     * @param id Id of the user that its being search
+     * @return User
+     */
     User getUserById(UUID id);
+
+    /**
+     * Update the User with the new values, the user must contain the Id of the user to update
+     * @param user User Object with the new values to update, it must contain the Id of the user to update
+     * @return User
+     */
     User updateUser(User user);
+
+    /**
+     * Adds a new student
+     * @param student Student Object to add, it must contain all the required fields to create a student
+     * @return Student
+     */
     Student addStudent(Student student);
+    /**
+     * Adds a new professor
+     * @param professor Professor Object to add, it must contain all the required fields to create a professor
+     * @return Professor
+     */
     Professor addProfessor(Professor professor);
+    /**
+     * Adds a new admin
+     * @param admin Admin Object to add, it must contain all the required fields to create an admin
+     * @return Admin
+     */
     Admin addAdmin(Admin admin);
 }
