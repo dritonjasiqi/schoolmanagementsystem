f3b2b78ebc46655340849370972c8cfbbb3c1ac4
jasiq <jasiqidriton@gmail.com>
2026-06-12 01:28:10 +0200

    Wrote Javadoc for JwtService.java Methods

--- backend/src/main/java/com/auroraschool/backend/security/JwtService.java
+++ backend/src/main/java/com/auroraschool/backend/security/JwtService.java
@@ -28,47 +28,99 @@ import java.util.Map;
 import java.util.function.Function;
 
 /**
- * Service for JWT.
+ * Service for handling JSON Web Token (JWT) operations, including
+ * parsing, generation, and validation for secure user authentication.
  */
 @Service // Marks class
 public class JwtService {
+
+    /**
+     * Secret Key. Only for Testing. Real World Application should be handled with an environment variable.
+     */
     @Value("${application.security.jwt.secret-key:404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970}")
     private String secretKey;
 
+    /**
+     * Extract the username from the provided JWT
+     * @param token JWT Token
+     * @return username contained in Token
+     */
     public String extractUsername(String token){
         return extractClaim(token, Claims::getSubject);
     }
 
+    /**
+     * Decodes the secret Key and generates a signing key for Verification of the token
+     * @return Cryptographic Key Object
+     */
     private Key getSignInKey(){
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
+    /**
+     * Parses the token to retrieve all claims stored within.
+     * @param token JWT Token
+     * @return Claims set from the token
+     */
     private Claims extractAllClaims(String token){
         return Jwts.parserBuilder().setSigningKey(getSignInKey()).build().parseClaimsJws(token).getBody();
     }
 
+    /**
+     * Extracts a specific claim from the token with help of a functional Interface
+     * @param token JWT token
+     * @param claimsResolver Function to extract the specific claim
+     * @return The requested claim
+     */
     public <T> T extractClaim(String token, Function<Claims,T> claimsResolver){
         final Claims claim = extractAllClaims(token);
         return claimsResolver.apply(claim);
     }
 
+    /**
+     * Generates a new Token for the specified user details
+     * @param userDetails The user to generate the Token for
+     * @return A signed JWT string
+     */
     public String generateToken(UserDetails userDetails){
         return generateToken(new HashMap<>(), userDetails);
     }
 
+    /**
+     * Internal method to generate a token with optional extra claims.
+     * @param extraClaims A map of additional claims to include in the token.
+     * @param userDetails The user details.
+     * @return A signed JWT string.
+    */
     private String generateToken(Map<String, Object> extraClaims, UserDetails userDetails){
         return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername()).setIssuedAt(new Date(System.currentTimeMillis())).setExpiration(new Date(System.currentTimeMillis() + 1000 *60 *24)).signWith(getSignInKey(), SignatureAlgorithm.HS256).compact();
     }
 
+    /**
+     * Extract the expiration Date from the Token
+     * @param token JWT Token
+     * @return Expiration Date
+     */
     private Date extractExpiration(String token){
         return extractClaim(token, Claims::getExpiration);
     }
 
+    /**
+     * Checks if the token has expired
+     * @param token JWT Token
+     * @return True if token is expired, orElse False
+     */
     private boolean isTokenExpired(String token){
         return extractExpiration(token).before(new Date());
     }
 
+    /**
+     * Checks if the token belongs to the User and hasnt expired
+     * @param token JWT Token
+     * @param userDetails The user in which will be applied the Validation
+     * @return True if valid, orElse False
+     */
     private boolean isTokenValid(String token, UserDetails userDetails){
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername()))  && !isTokenExpired(token);
