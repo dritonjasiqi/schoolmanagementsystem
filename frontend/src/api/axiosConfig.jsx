9f4079c4e311894930dbe2d2e822073c9d71a425
driton <jasiqidriton@gmail.com>
2026-06-22 01:43:37 +0200

    Created the APi to connect Frontend with Backend

---
+++ frontend/src/api/axiosConfig.jsx
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const myApi = axios.create({
+    baseURL: 'http://localhost:8080/api',
+});
+
+myApi.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('jwt_token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    }, (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default myApi;
