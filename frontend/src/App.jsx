b835df2cd8dc05c4a50722bd26d56e53dbf03c5f
driton <jasiqidriton@gmail.com>
2026-07-09 01:22:05 +0200

    Added a simple Protected Route, will get roles based system really fast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,33 +16,34 @@ import Class from './pages/classes/Class';
 import EditClass from './pages/classes/EditClass';
 import RemoveClass from './pages/classes/RemoveClass';
 import AddClass from './pages/classes/AddClass';
+import ProtectedRoute from './components/ProtectedRoute';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Layout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="/dashboard/userpages/users" element={<Users />} />
-          <Route path="/dashboard/settings" element={<Settings />} />
-          {/* Placeholder routes for dropdown sub-items */}
-          <Route path="/dashboard/products/*" element={<div className="p-4">Products Module</div>} />
-          <Route path="/dashboard/reports/*" element={<div className="p-4">Reports Module</div>} />
-          <Route path="/dashboard/userpages/addProfessor" element={<AddProfessor />} />
-          <Route path="/dashboard/userpages/professors" element={<Professors />} />
-          <Route path="/dashboard/classes/classes" element={<Class />} />
-          <Route path="/dashboard/classes/addClass" element={<AddClass />} />
-        </Route>
-        <Route path="/classes/editClass/:id" element={<EditClass />} />
-        <Route path="/classes/removeClass/:id" element={<RemoveClass />} />
-        <Route path="/userpages/editProfessor/:id" element={<EditProfessor />} />
-        <Route path="/userpages/removeProfessor/:id" element={<RemoveProfessor />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/aboutus" element={<AboutUs />} />
-
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="/dashboard/userpages/users" element={<Users />} />
+            <Route path="/dashboard/settings" element={<Settings />} />
+            <Route path="/dashboard/products/*" element={<div className="p-4">Products Module</div>} />
+            <Route path="/dashboard/reports/*" element={<div className="p-4">Reports Module</div>} />
+            <Route path="/dashboard/userpages/addProfessor" element={<AddProfessor />} />
+            <Route path="/dashboard/userpages/professors" element={<Professors />} />
+            <Route path="/dashboard/classes/classes" element={<Class />} />
+            <Route path="/dashboard/classes/addClass" element={<AddClass />} />
+          </Route>
+          <Route path="/classes/editClass/:id" element={<EditClass />} />
+          <Route path="/classes/removeClass/:id" element={<RemoveClass />} />
+          <Route path="/userpages/editProfessor/:id" element={<EditProfessor />} />
+          <Route path="/userpages/removeProfessor/:id" element={<RemoveProfessor />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
